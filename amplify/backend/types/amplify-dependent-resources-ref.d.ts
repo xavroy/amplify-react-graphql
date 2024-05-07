dc513027b3f5406f661eed4e0f7bb7812e85e27c
Xavier ROY <xavier@shawan.eu>
2024-05-07 18:54:31 +0200

    Added frontend code

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "amplifyapp": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
   "auth": {
     "amplifyappd5ec3e13": {
       "AppClientID": "string",
