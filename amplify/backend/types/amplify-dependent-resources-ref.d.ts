c1ff82b7f9019ec7d8c5386faad95f9587074e2e
Xavier ROY <xavier@shawan.eu>
2024-05-07 19:08:33 +0200

    Added graphql and storage

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -16,5 +16,11 @@ export type AmplifyDependentResourcesAttributes = {
       "UserPoolId": "string",
       "UserPoolName": "string"
     }
+  },
+  "storage": {
+    "s31607e9e2": {
+      "BucketName": "string",
+      "Region": "string"
+    }
   }
 }
